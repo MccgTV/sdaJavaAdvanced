3bc6cf8debe7a80f0394cb895dce730e83cbd699
MccgTV <mccgtv@gmail.com>
2021-03-14 15:50:29 +0100

    Typy wyliczeniowe update

---
+++ src/main/java/com/sda/generic/Box.java
@@ -0,0 +1,17 @@
+package com.sda.generic;
+
+public class Box<T> {
+
+    T item;
+
+
+    T getItem() {
+        return item;
+    }
+
+    void setItem(T item) {
+        this.item = item;
+    }
+
+
+}
---
+++ src/main/java/com/sda/generic/BoxApp.java
@@ -0,0 +1,20 @@
+package com.sda.generic;
+
+public class BoxApp {
+
+    public static void main(String[] args) {
+
+        Box<Integer> boxLiczba = new Box<Integer>();
+        boxLiczba.setItem(23);
+
+        Box<String> boxTekst = new Box<String>();
+        boxTekst.setItem("tekst");
+
+        Box<Double> boxDouble = new Box<Double>();
+        boxDouble.setItem(2.23);
+
+        System.out.println(boxLiczba.getItem());
+        System.out.println(boxTekst.getItem());
+        System.out.println(boxDouble.getItem());
+    }
+}
